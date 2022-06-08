97c590b122fa78b01ed97fb55718ee4b75ffbc81
erynder-z <stfn.bgr@gmail.com>
2022-06-08 21:36:05 +0200

    Create Sidebar component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Lorem.</h3>
+      <h3>Lorem, side.</h3>
+      <h3>Lorem.</h3>
+      <h3>Lorem.</h3>
+      <h3>Lorem, side.</h3>
+    </div>
+  );
+}
+
+export default Sidebar;
