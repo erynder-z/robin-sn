888801433c1530c48f9eafa9036e642bfd732e08
erynder-z <stfn.bgr@gmail.com>
2022-06-08 21:10:29 +0200

    Add temp styling

--- src/components/CreateUserAccount/CreateUserAccount.jsx
+++ src/components/CreateUserAccount/CreateUserAccount.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import './CreateUserAccount.css';
 import { doc, setDoc } from 'firebase/firestore';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import resizeFile from '../../helpers/ImageResizer/ImageResizer';
 import { database } from '../Firebase/Firebase';
 import placeholder from '../../assets/placeholder.png';
@@ -64,44 +65,12 @@ function CreateUserAccount({ user }) {
     <div className="create-user-account">
       <div className="create-user-container">
         <img src={userObject.userPic} alt="avatar" />
-      </div>
-      <form>
-        <div className="input-container">
-          <label htmlFor="uname">
-            Username
-            <input
-              type="text"
-              placeholder="enter user name"
-              value={userObject.username}
-              onChange={(e) => {
-                setUserObject((prevState) => ({
-                  ...prevState,
-                  username: e.target.value
-                }));
-              }}
-              required
-            />
-          </label>
-        </div>
-        <textarea
-          className="description-input"
-          type="text"
-          placeholder="description"
-          value={userObject.description}
-          onChange={(e) => {
-            setUserObject((prevState) => ({
-              ...prevState,
-              description: e.target.value
-            }));
-          }}
-          required
-        />
-
-        <label htmlFor="picture">
+        <label htmlFor="picture" className="custom-file-upload-label">
           Upload picture
           <input
             className="custom-file-upload"
             type="file"
+            id="picture"
             name="picture"
             accept="image/png, image/jpeg"
             onChange={(e) => {
@@ -109,23 +78,61 @@ function CreateUserAccount({ user }) {
             }}
           />
         </label>
-      </form>
-      <div className="button-container">
-        <button
-          type="submit"
-          className="loginBtn"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-          onKeyDown={(e) => {
-            handleSubmit(e);
-          }}
-          tabIndex={0}>
-          Lets go!
-        </button>
+        <form>
+          <div className="input-container">
+            <label htmlFor="uname">
+              Username
+              <input
+                type="text"
+                placeholder="enter user name"
+                value={userObject.username}
+                onChange={(e) => {
+                  setUserObject((prevState) => ({
+                    ...prevState,
+                    username: e.target.value
+                  }));
+                }}
+                required
+              />
+            </label>
+          </div>
+          <textarea
+            className="description-input"
+            type="text"
+            placeholder="write a little bit about yourself"
+            value={userObject.description}
+            onChange={(e) => {
+              setUserObject((prevState) => ({
+                ...prevState,
+                description: e.target.value
+              }));
+            }}
+            required
+          />
+        </form>
       </div>
+
+      <button
+        type="submit"
+        className="createBtn"
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+        onKeyDown={(e) => {
+          handleSubmit(e);
+        }}
+        tabIndex={0}>
+        Lets go!
+      </button>
     </div>
   );
 }
 
 export default CreateUserAccount;
+
+CreateUserAccount.propTypes = {
+  user: PropTypes.shape({
+    uid: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired
+  }).isRequired
+};
