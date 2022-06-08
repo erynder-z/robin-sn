b925416f380d4619150ac003cc510c579c219bdd
erynder-z <stfn.bgr@gmail.com>
2022-06-08 21:06:51 +0200

    Fix missing props

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { database, auth } from './components/Firebase/Firebase';
+import { auth } from './components/Firebase/Firebase';
 import LoginScreen from './components/LoginScreen/LoginScreen';
 import Start from './components/Start/Start';
 import Home from './components/Home/Home';
@@ -13,14 +13,16 @@ function App() {
 
   return (
     <div className="App">
-      {user ? <Start user={user} /> : <LoginScreen />}{' '}
       <Routes>
-        <Route path="/start" element={!user ? <Navigate replace to="/" /> : <Start />} />
-        <Route path="/login" element={!user ? <Navigate replace to="/" /> : <LoginScreen />} />
-        <Route path="/home" element={!user ? <Home replace to="/" /> : <Home />} />
+        <Route
+          path="/start"
+          element={!user ? <Navigate replace to="/" /> : <Start user={user} />}
+        />
+        <Route path="/" element={!user ? <LoginScreen /> : <Start user={user} />} />
+        <Route path="/home" element={!user ? <Navigate replace to="/" /> : <Home />} />
         <Route
           path="/createaccount"
-          element={!user ? <Home replace to="/" /> : <CreateUserAccount />}
+          element={!user ? <Navigate replace to="/" /> : <CreateUserAccount user={user} />}
         />
       </Routes>
     </div>
