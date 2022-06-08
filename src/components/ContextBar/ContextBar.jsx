4f7375acb01073f712f0ec14aacce41c794b33c1
erynder-z <stfn.bgr@gmail.com>
2022-06-08 21:35:53 +0200

    Create ContextBar component

---
+++ src/components/ContextBar/ContextBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ContextBar.css';
+
+function ContextBar() {
+  return (
+    <div className="contextbar">
+      <h3>Lorem.</h3>
+      <h3>Lorem, context.</h3>
+      <h3>Lorem.</h3>
+      <h3>Lorem.</h3>
+      <h3>Lorem, context.</h3>
+    </div>
+  );
+}
+
+export default ContextBar;
