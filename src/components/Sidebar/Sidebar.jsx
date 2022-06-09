1125fb7491a6789c6784646044353f1047cedc6a
erynder-z <stfn.bgr@gmail.com>
2022-06-09 10:39:47 +0200

    Simple sidebar styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import './Sidebar.css';
+import {
+  BiHomeAlt,
+  BiHash,
+  BiBell,
+  BiEnvelope,
+  BiBookmark,
+  BiListUl,
+  BiUserCircle
+} from 'react-icons/bi';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h3>Lorem.</h3>
-      <h3>Lorem, side.</h3>
-      <h3>Lorem.</h3>
-      <h3>Lorem.</h3>
-      <h3>Lorem, side.</h3>
+      <ul>
+        <li className="sidebar-item">
+          <BiHomeAlt size="2rem" />
+          <span>Home</span>
+        </li>
+        <li className="sidebar-item">
+          <BiHash size="2rem" />
+          <span>Explore</span>
+        </li>
+        <li className="sidebar-item">
+          <BiBell size="2rem" />
+          <span>Notifications</span>
+        </li>
+        <li className="sidebar-item">
+          <BiEnvelope size="2rem" />
+          <span>Messages</span>
+        </li>
+        <li className="sidebar-item">
+          <BiBookmark size="2rem" />
+          <span>Bookomarks</span>
+        </li>
+        <li className="sidebar-item">
+          <BiListUl size="2rem" />
+          <span>Lists</span>
+        </li>
+        <li className="sidebar-item">
+          <BiUserCircle size="2rem" />
+          <span>Profile</span>
+        </li>
+      </ul>
     </div>
   );
 }
