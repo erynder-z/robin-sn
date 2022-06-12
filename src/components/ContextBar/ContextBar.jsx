8116780cb6d1a518737daf0495f0ba54058925be
erynder-z <stfn.bgr@gmail.com>
2022-06-12 20:42:27 +0200

    Render user list inside context bar

--- src/components/ContextBar/ContextBar.jsx
+++ src/components/ContextBar/ContextBar.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
+import { database } from '../Firebase/Firebase';
 import './ContextBar.css';
 
 function ContextBar() {
+  const [userList, setUserList] = useState([]);
+
+  const getSomeUsers = async () => {
+    const querySnapshot = await getDocs(collection(database, 'users'));
+
+    const list = [];
+    querySnapshot.forEach((doc) => {
+      list.push({
+        userID: doc.data().userID,
+        username: doc.data().username,
+        userPic: doc.data().userPic
+      });
+      setUserList(list);
+    });
+  };
+
+  useEffect(() => {
+    getSomeUsers();
+  }, []);
+
+  const userItem = (usr) => (
+    <div className="userlist-item" key={usr.userID}>
+      <img className="userlist-usrpic" src={usr.userPic} alt="user avatar" />
+      <div className="userlist-username">@{usr.username}</div>
+      <div className="userlist-follow">follow</div>
+    </div>
+  );
+
   return (
     <div className="contextbar">
-      <h3>Lorem.</h3>
-      <h3>Lorem, context.</h3>
-      <h3>Lorem.</h3>
-      <h3>Lorem.</h3>
-      <h3>Lorem, context.</h3>
+      <div className="userlist">{userList.map((user) => userItem(user))}</div>
     </div>
   );
 }
