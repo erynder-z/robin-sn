40cceccf19ff2b7fdf8b8d2163132892eec74217
erynder-z <stfn.bgr@gmail.com>
2022-06-24 11:25:53 +0200

    Move follow-user-list to own component

---
+++ src/components/FollowUserList/FollowUserList.jsx
@@ -0,0 +1,135 @@
+import { arrayRemove, arrayUnion, collection, doc, getDocs, updateDoc } from 'firebase/firestore';
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { BiUserPlus, BiUserMinus } from 'react-icons/bi';
+import { database } from '../Firebase/Firebase';
+import './FollowUserList.css';
+
+function FollowUserList({ userData }) {
+  const { userID, following } = userData;
+  const [userList, setUserList] = useState([]);
+
+  // get all list of all users in the datababse
+  // TODO: limit number of users
+  const getUserList = async () => {
+    const querySnapshot = await getDocs(collection(database, 'users'));
+
+    const list = [];
+    querySnapshot.forEach((document) => {
+      // for each user: check if we are already following that user
+      const checkIfAlreadyFollowing = (usr, followUsr) => {
+        const followingList = usr.following;
+
+        if (followingList.some((e) => e.userID === followUsr)) {
+          return true;
+        }
+        return false;
+      };
+      if (document.data().userID !== userData.userID) {
+        list.push({
+          userID: document.data().userID,
+          username: document.data().username,
+          userPic: document.data().userPic,
+          following: checkIfAlreadyFollowing(userData, document.data().userID)
+        });
+      }
+      setUserList(list);
+    });
+  };
+
+  useEffect(() => {
+    getUserList();
+  }, [following]);
+
+  const follow = async (followUserID) => {
+    const userToFollowRef = doc(database, 'users', followUserID);
+    const userThatFollowsRef = doc(database, 'users', userID);
+
+    await updateDoc(userToFollowRef, {
+      followers: arrayUnion({ userID })
+    });
+    await updateDoc(userThatFollowsRef, {
+      following: arrayUnion({ userID: followUserID })
+    });
+  };
+
+  const unFollow = async (followUserID) => {
+    const userToFollowRef = doc(database, 'users', followUserID);
+    const userThatFollowsRef = doc(database, 'users', userID);
+
+    await updateDoc(userToFollowRef, {
+      followers: arrayRemove({ userID })
+    });
+    await updateDoc(userThatFollowsRef, {
+      following: arrayRemove({ userID: followUserID })
+    });
+  };
+
+  const userItem = (usr) => (
+    <div className="userlist-item" key={usr.userID}>
+      <img className="userlist-usrpic" src={usr.userPic} alt="user avatar" />
+      <div className="userlist-username">@{usr.username}</div>
+      {!usr.following && (
+        <div
+          className="userlist-follow"
+          role="button"
+          tabIndex={0}
+          onClick={() => {
+            follow(usr.userID);
+          }}
+          onKeyDown={() => {
+            follow(usr.userID);
+          }}>
+          <BiUserPlus size="1.5rem" />
+        </div>
+      )}
+      {usr.following && (
+        <div
+          className="userlist-unfollow"
+          role="button"
+          tabIndex={0}
+          onClick={() => {
+            unFollow(usr.userID);
+          }}
+          onKeyDown={() => {
+            unFollow(usr.userID);
+          }}>
+          <BiUserMinus size="1.5rem" />
+        </div>
+      )}
+    </div>
+  );
+
+  return <div className="userlist">{userList.map((user) => userItem(user))}</div>;
+}
+
+export default FollowUserList;
+
+FollowUserList.propTypes = {
+  userData: PropTypes.shape({
+    userID: PropTypes.string.isRequired,
+    isSetup: PropTypes.bool.isRequired,
+    username: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    userPic: PropTypes.string.isRequired,
+    useremail: PropTypes.string.isRequired,
+    joined: PropTypes.objectOf(PropTypes.number),
+    followers: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    following: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    posts: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    replies: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    reposts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    likes: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    bookmarks: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired
+  }).isRequired
+};
