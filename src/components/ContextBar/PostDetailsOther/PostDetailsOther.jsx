d617b6bb9b186a63ef35c0b3bd111fcb4771e608
erynder-z <stfn.bgr@gmail.com>
2022-06-24 12:45:04 +0200

    Add context menu for posts

---
+++ src/components/ContextBar/PostDetailsOther/PostDetailsOther.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './PostDetailsOther.css';
+
+function PostDetailsOther({ userData }) {
+  return (
+    <div className="postDetails-other">
+      OTHER ipsum dolor sit amet, consectetur adipisicing elit. Eaque officia molestias aspernatur
+      quidem. Repellat quos saepe quo omnis commodi autem eligendi consequatur voluptate molestias
+      distinctio?
+    </div>
+  );
+}
+
+export default PostDetailsOther;
+
+PostDetailsOther.propTypes = {
+  userData: PropTypes.shape({
+    userID: PropTypes.string.isRequired,
+    isSetup: PropTypes.bool.isRequired,
+    username: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    userPic: PropTypes.string.isRequired,
+    useremail: PropTypes.string.isRequired,
+    joined: PropTypes.objectOf(PropTypes.number),
+    followers: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    following: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    posts: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    replies: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    reposts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    likes: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    bookmarks: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired
+  }).isRequired
+};
