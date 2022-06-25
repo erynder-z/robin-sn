8d905a50632668e888a40fc2840b1be8aa8c15d8
erynder-z <stfn.bgr@gmail.com>
2022-06-25 15:56:02 +0200

    Create bookmarks component

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './Bookmarks.css';
+import PostItem from '../PostItem/PostItem';
+
+function Bookmarks({ userData }) {
+  const [bookmarks, setBookmarks] = useState(null);
+
+  const sortPosts = (lst) => {
+    const unsorted = [];
+    lst.map((o) =>
+      unsorted.push({ postID: o.postID, created: o.created, userID: userData.userID })
+    );
+    const sorted = unsorted.sort((a, b) => (a.created.seconds < b.created.seconds ? 1 : -1));
+    return sorted;
+  };
+
+  useEffect(() => {
+    setBookmarks(userData.bookmarks);
+  }, []);
+
+  return (
+    <div className="bookmarks-container">
+      <div className="bookmarks-header">Bookmarks</div>
+      <div className="bookmarks-content">
+        <div className="posts">
+          {bookmarks &&
+            sortPosts(bookmarks).map((post) => (
+              <PostItem key={post.postID} postID={post.postID} userID={userData.userID} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Bookmarks;
+
+Bookmarks.propTypes = {
+  userData: PropTypes.shape({
+    userID: PropTypes.string.isRequired,
+    isSetup: PropTypes.bool.isRequired,
+    username: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    userPic: PropTypes.string.isRequired,
+    useremail: PropTypes.string.isRequired,
+    joined: PropTypes.objectOf(PropTypes.number),
+    followers: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    following: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    posts: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    replies: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired,
+    reposts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    likes: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+    bookmarks: PropTypes.arrayOf(
+      PropTypes.shape({
+        created: PropTypes.objectOf(PropTypes.number),
+        postID: PropTypes.string
+      })
+    ).isRequired
+  }).isRequired
+};
