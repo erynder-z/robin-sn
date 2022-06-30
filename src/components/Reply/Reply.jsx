641d47c9f4556e9517ec0645b7492093ae317634
erynder-z <stfn.bgr@gmail.com>
2022-06-30 18:00:52 +0200

    Show userpic & enable emoji-input

--- src/components/Reply/Reply.jsx
+++ src/components/Reply/Reply.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import uniqid from 'uniqid';
+import Picker from 'emoji-picker-react';
 import './Reply.css';
+import { MdOutlineEmojiEmotions } from 'react-icons/md';
 import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore';
 import { database } from '../Firebase/Firebase';
 
-function Reply({ postID, userID, replyMode, toggleReplyModal, postOwner }) {
+function Reply({ postID, userID, userPic, replyMode, toggleReplyModal, postOwner }) {
   const [mode, setMode] = useState('');
   const [text, setText] = useState('');
   const [replyUserName, setReplyUserName] = useState('');
   const [showReplyUserName, setShowReplyUserName] = useState(false);
   const [fadeModal, setFadeModal] = useState(false);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   // add replied post's ID to user object
   const addPostToUserObject = async () => {
@@ -47,9 +50,15 @@ function Reply({ postID, userID, replyMode, toggleReplyModal, postOwner }) {
     }
   };
 
+  // add emoji from picker to text
+  const onEmojiClick = (event, emojiObject) => {
+    setShowEmojiPicker(false);
+    setText(text + emojiObject.emoji);
+  };
+
   useEffect(() => {
     setMode(replyMode);
-    setReplyUserName(postOwner.username);
+    setReplyUserName({ username: postOwner.username, userpic: postOwner.userpic });
   }, []);
 
   // show a modal in order to reply
@@ -86,23 +95,58 @@ function Reply({ postID, userID, replyMode, toggleReplyModal, postOwner }) {
         onKeyDown={(e) => {
           e.stopPropagation();
         }}>
-        <div className="replyModal-upper">
-          <div className="replyTo"> Replying to @{postOwner.username}</div>
+        <div className="replyModal-left">
+          {' '}
+          <img className="reply-userpic" src={replyUserName.userpic} alt="user avatar" />
+          <div className="reply-userpic-divider" />
+          <img className="reply-userpic" src={userPic} alt="user avatar" />
         </div>
-        <textarea
-          className="reply-modal-textarea"
-          cols="30"
-          rows="5"
-          placeholder="write your reply"
-        />
-        <button
-          className="replyBtn"
-          type="submit"
-          onClick={() => {
-            reply(postID, userID);
-          }}>
-          Reply
-        </button>
+        <div className="replyModal-right">
+          <div className="replyModal-upper">
+            <div className="replyTo"> Replying to @{replyUserName.username}</div>
+          </div>
+          <textarea
+            className="reply-modal-textarea"
+            cols="30"
+            rows="5"
+            placeholder="write your reply"
+          />
+          <div className="replyModal-post">
+            <MdOutlineEmojiEmotions
+              size="2rem"
+              className="show-emoji-picker"
+              onClick={() => {
+                setShowEmojiPicker(true);
+              }}
+            />
+            {showEmojiPicker && (
+              <div className="emoji-picker-overlay">
+                <div
+                  className="emoji-picker-close"
+                  role="button"
+                  tabIndex={0}
+                  onClick={() => {
+                    setShowEmojiPicker(false);
+                  }}
+                  onKeyDown={() => {
+                    setShowEmojiPicker(false);
+                  }}>
+                  {' '}
+                  &times;
+                </div>
+                <Picker onEmojiClick={onEmojiClick} disableSearchBar />
+              </div>
+            )}
+            <button
+              className="replyBtn-modal"
+              type="submit"
+              onClick={() => {
+                reply(postID, userID);
+              }}>
+              Reply
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -111,32 +155,63 @@ function Reply({ postID, userID, replyMode, toggleReplyModal, postOwner }) {
   const ReplyAppend = (
     <div className="replyAppend-body">
       <div className={`replyTo-append ${!showReplyUserName ? 'hidden' : ''}`}>
-        replying to: @{replyUserName}
+        replying to: @{replyUserName.username}
       </div>
-      <textarea
-        className="reply-append-textarea"
-        cols="30"
-        rows="5"
-        placeholder="write your reply"
-        value={text}
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
-        onFocus={() => {
-          setShowReplyUserName(true);
-        }}
-        onBlur={() => {
-          setShowReplyUserName(false);
-        }}
-      />
-      <button
-        className="postBtn"
-        type="submit"
-        onClick={() => {
-          reply(postID, userID);
-        }}>
-        Reply
-      </button>
+      <div className="replyAppend-wrapper">
+        <img className="reply-userpic-append" src={userPic} alt="user avatar" />
+        <textarea
+          className="reply-append-textarea"
+          cols="30"
+          rows="5"
+          placeholder="write your reply"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          onFocus={() => {
+            setShowReplyUserName(true);
+          }}
+          onBlur={() => {
+            setShowReplyUserName(false);
+          }}
+        />
+      </div>
+      <div className="replyAppend-post">
+        <MdOutlineEmojiEmotions
+          size="2rem"
+          className="show-emoji-picker"
+          onClick={() => {
+            setShowEmojiPicker(true);
+          }}
+        />
+        {showEmojiPicker && (
+          <div className="emoji-picker-overlay">
+            <div
+              className="emoji-picker-close"
+              role="button"
+              tabIndex={0}
+              onClick={() => {
+                setShowEmojiPicker(false);
+              }}
+              onKeyDown={() => {
+                setShowEmojiPicker(false);
+              }}>
+              {' '}
+              &times;
+            </div>
+            <Picker onEmojiClick={onEmojiClick} disableSearchBar />
+          </div>
+        )}
+
+        <button
+          className="replyBtn-append"
+          type="submit"
+          onClick={() => {
+            reply(postID, userID);
+          }}>
+          Reply
+        </button>
+      </div>
     </div>
   );
 
@@ -158,5 +233,6 @@ Reply.propTypes = {
   postOwner: PropTypes.shape({
     username: PropTypes.string.isRequired,
     userpic: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  userPic: PropTypes.string.isRequired
 };
