0aec1c9272be27deb45f9b5811b70a4f46451ee1
erynder-z <stfn.bgr@gmail.com>
2022-07-12 20:51:29 +0200

    Display unread dm buble notification

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   TbHome2,
@@ -16,6 +16,16 @@ import './Sidebar.css';
 
 function Sidebar({ activeTab, logout }) {
   const { userData } = GetUserContext();
+  const [unreadMessagesExist, setUnreadMessagesExist] = useState(false);
+
+  const checkForUnreadMessages = () => {
+    const list = [...userData.messages];
+    return list.some((msg) => msg.isRead === false);
+  };
+
+  useEffect(() => {
+    setUnreadMessagesExist(checkForUnreadMessages());
+  }, [userData.messages]);
 
   return (
     <div className="sidebar">
@@ -23,38 +33,51 @@ function Sidebar({ activeTab, logout }) {
         <img className="logo" src={logo} alt="app logo" />
         <Link to="/main/home">
           <li className={`sidebar-item ${activeTab === 'home' ? 'active' : 'inactive'}`}>
-            <TbHome2 size="2rem" />
-            <span>Home</span>
+            <div className="sidebar-item-wrapper">
+              <TbHome2 size="2rem" />
+              <span>Home</span>
+            </div>
           </li>
         </Link>
         <Link to="/main/explore">
           <li className={`sidebar-item ${activeTab === 'explore' ? 'active' : 'inactive'}`}>
-            <TbHash size="2rem" />
-            <span>Explore</span>
+            <div className="sidebar-item-wrapper">
+              <TbHash size="2rem" />
+              <span>Explore</span>
+            </div>
           </li>
         </Link>
         <Link to="/main/mentions">
           <li className={`sidebar-item ${activeTab === 'mentions' ? 'active' : 'inactive'}`}>
-            <TbSpeakerphone size="2rem" />
-            <span>Mentions</span>
+            <div className="sidebar-item-wrapper">
+              <TbSpeakerphone size="2rem" />
+              <span>Mentions</span>
+            </div>
           </li>
         </Link>
         <Link to="/main/directmessages">
           <li className={`sidebar-item ${activeTab === 'directmessages' ? 'active' : 'inactive'}`}>
-            <TbMessageCircle size="2rem" />
-            <span>DMs</span>
+            <div className="sidebar-item-wrapper">
+              <TbMessageCircle size="2rem" />
+              <span>DMs</span>
+              {unreadMessagesExist && <div className="notificationBubble" />}
+            </div>
           </li>
         </Link>
         <Link to="/main/bookmarks">
           <li className={`sidebar-item ${activeTab === 'bookmarks' ? 'active' : 'inactive'}`}>
-            <TbBookmarks size="2rem" />
-            <span>Bookmarks</span>
+            <div className="sidebar-item-wrapper">
+              <TbBookmarks size="2rem" />
+              <span>Bookmarks</span>
+            </div>
           </li>
         </Link>
         <Link to="/main/myprofile">
           <li className={`sidebar-item ${activeTab === 'myprofile' ? 'active' : 'inactive'}`}>
-            <TbUser size="2rem" />
-            <span>Profile</span>
+            <div className="sidebar-item-wrapper">
+              <TbUser size="2rem" />
+              <span>Profile</span>
+            </div>
           </li>
         </Link>
         <UserInfo logout={logout} userData={userData} />
