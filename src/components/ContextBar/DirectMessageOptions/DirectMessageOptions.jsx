a0958401d4e59ffd24cbd4ab20b5a75b62a7a952
erynder-z <stfn.bgr@gmail.com>
2022-07-12 21:23:49 +0200

    Mark all messages as read

--- src/components/ContextBar/DirectMessageOptions/DirectMessageOptions.jsx
+++ src/components/ContextBar/DirectMessageOptions/DirectMessageOptions.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BsBookmarkPlus } from 'react-icons/bs';
+import { BiCheckDouble } from 'react-icons/bi';
 import './DirectMessageOptions.css';
+import { doc, updateDoc } from 'firebase/firestore';
+import { GetUserContext } from '../../../contexts/UserContext';
+import { database } from '../../Firebase/Firebase';
 
 function DirectMessageOptions({ showWarning }) {
-  console.log(showWarning);
+  const { userData } = GetUserContext();
+
+  const markMessagesAsRead = async () => {
+    const getMessageList = async () => {
+      const list = [...userData.messages];
+      const newList = [];
+      list.forEach((msg) => {
+        newList.push({ ...msg, isRead: true });
+      });
+      return newList;
+    };
+
+    const updatedMessageList = await getMessageList();
+    const userRef = doc(database, 'users', userData.userID);
+    try {
+      await updateDoc(userRef, {
+        messages: updatedMessageList
+      });
+    } catch (err) {
+      showWarning(err.message);
+    }
+  };
+
   return (
     <div className="directMessages fadein">
       <div className="directMessages-context-header">Options</div>
-      <div className="something">
+      <div className="markMessagesRead">
         <div
-          className="something-something"
+          className="message-markRead"
           role="button"
           tabIndex={0}
           onClick={() => {
-            //
+            markMessagesAsRead();
           }}
           onKeyDown={() => {
-            //
+            markMessagesAsRead();
           }}>
-          <BsBookmarkPlus size="2rem" className="something-something-icon" />
-          Something
+          <BiCheckDouble size="2rem" className="message-markRead-icon" />
+          Mark all as read
         </div>
       </div>
     </div>
