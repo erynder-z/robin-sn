33e9bb1d56a3af8e3d9bde5011fe9bbd6bdec231
erynder-z <stfn.bgr@gmail.com>
2022-08-07 19:00:37 +0200

    Fix regex not working on Safari

--- src/helpers/HashtagCreator/HashtagCreator.jsx
+++ src/helpers/HashtagCreator/HashtagCreator.jsx
@@ -4,8 +4,9 @@ const parseHashtag = async (text) => {
   // return an array with all strings preceeded by a "#" from passed in text
   const hashify = (txt) => {
     const hashtagArray = [];
-    txt.replace(/(?<=#)\S+\b/g, (ht) => {
-      hashtagArray.push(ht.toLowerCase());
+    /*  txt.replace(/(?<=#)\S+\b/g, (ht) => { */
+    txt.replace(/#[A-Za-z0-9_]+/, (ht) => {
+      hashtagArray.push(ht.substring(1).toLowerCase());
     });
     return hashtagArray;
   };
--- src/helpers/MentionCreator/MentionCreator.jsx
+++ src/helpers/MentionCreator/MentionCreator.jsx
@@ -3,8 +3,9 @@ const parseMention = async (text) => {
   const mention = (txt) => {
     const mentionsArray = [];
     // match string after "@" until next whitespace. positive lookbehind to exclude the "@". word boundary to ignore special characters after the string..
-    txt.replace(/(?<=@)\S+\b/g, (m) => {
-      mentionsArray.push(m.toLowerCase());
+    /*    txt.replace(/(?<=@)\S+\b/g, (m) => { */
+    txt.replace(/@[A-Za-z0-9_]+/, (m) => {
+      mentionsArray.push(m.substring(1).toLowerCase());
     });
     return mentionsArray;
   };
